93f53bc1c88b195fac69e3cd99831b0a98affa2b
Guiguishow <unconfigured@null.spigotmc.org>
2024-10-28 09:50:32 +0100

    Création des events pour itemframe invi, et doors/trapdoors en fer

---
+++ src/main/java/onl/tesseract/event/InvisibleItemFrameHandler.java
@@ -0,0 +1,33 @@
+package onl.tesseract.event;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InvisibleItemFrameHandler implements Listener {
+
+    Map<Location, Integer> ticks = new HashMap<Location, Integer>();
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+
+    public void onClickFrame(PlayerInteractEntityEvent event) {
+        Bukkit.getServer().getCurrentTick();
+        if (event.getPlayer().isSneaking() && event.getRightClicked() instanceof ItemFrame itemFrame) {
+            int lastTick = ticks.getOrDefault(itemFrame.getLocation(),0);
+            if (lastTick != Bukkit.getServer().getCurrentTick()) {
+                itemFrame.setVisible(!itemFrame.isVisible());
+                event.setCancelled(true);
+                ticks.put(itemFrame.getLocation(), Bukkit.getServer().getCurrentTick());
+            }
+
+        }
+
+    }
+}
