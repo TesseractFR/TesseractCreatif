95cc12f01bda056107c395889035129fe7b93b46
aikiboo <florian.suchaud@hotmail.fr>
2024-11-17 17:24:28 +0100

    Ajout plugin kotlin jpa + nettoyage PlayerRankInfo

--- src/main/java/onl/tesseract/rank/entity/PlayerRankInfo.kt
+++ src/main/java/onl/tesseract/rank/entity/PlayerRankInfo.kt
@@ -1,26 +1,19 @@
 package onl.tesseract.rank.entity
 
 import jakarta.persistence.*
-import lombok.AccessLevel
-import lombok.Data
-import lombok.experimental.FieldDefaults
 import org.hibernate.annotations.JdbcTypeCode
 import java.sql.Types
 import java.util.*
 
 @Entity
 @Table(name = "t_player_ranks")
-@Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
 data class PlayerRankInfo (
     @Id
     @Column(updatable = false, nullable = false, columnDefinition = "VARCHAR(36)", unique = true)
     @JdbcTypeCode(
         Types.VARCHAR
     )
-    val uuid: UUID = UUID.randomUUID(),
-
-    var isVirtuose : Boolean = false,
+    val uuid: UUID,
 
     @Enumerated(EnumType.STRING)
     var playerRank: PlayerRank = PlayerRank.APPRENTI,
