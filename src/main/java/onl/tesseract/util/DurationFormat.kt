402ca5e7a7c010782c9572cf11f038c67d2be18e
GuiguiShow <g.eckenschwiller@gmail.com>
2024-11-22 14:19:39 +0100

    séparation commande en 2 classes

--- src/main/java/onl/tesseract/util/DurationFormat.kt
+++ src/main/java/onl/tesseract/util/DurationFormat.kt
@@ -6,16 +6,16 @@ import kotlin.math.floor
 object DurationFormat {
     @JvmStatic
     fun formatTime(duration: Duration): String {
-        val nbDays = duration.toDays();
-        val years = (nbDays/365.25).toInt();
-        val months = (nbDays/30.4375).toInt() % 12;
+        val nbDays = duration.toDays()
+        val years = (nbDays/365.25).toInt()
+        val months = (nbDays/30.4375).toInt() % 12
         val days = floor(nbDays%30.4375).toInt()
-        val hours = (duration.toHours() % 24);
+        val hours = (duration.toHours() % 24)
         val minutes = duration.toMinutes() % 60
         val seconds = duration.seconds % 60
-        val shours = if(hours < 10) "0$hours" else hours.toString();
-        val sminutes = if(minutes < 10) "0$minutes" else minutes.toString();
-        val sseconds = if(seconds < 10) "0$seconds" else seconds.toString();
+        val shours = if(hours < 10) "0$hours" else hours.toString()
+        val sminutes = if(minutes < 10) "0$minutes" else minutes.toString()
+        val sseconds = if(seconds < 10) "0$seconds" else seconds.toString()
         val parts = mutableListOf<String>()
         if (years > 0) parts.add("$years" + "a")
         if (months > 0 || years > 0) parts.add("$months" + "mo")
