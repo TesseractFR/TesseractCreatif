a4c5ad10b1443622713efea2cda47f25494aa6ba
GuiguiShow <g.eckenschwiller@gmail.com>
2024-11-24 15:06:50 +0100

    update

--- src/main/java/onl/tesseract/command/argument/TimeStringArg.kt
+++ src/main/java/onl/tesseract/command/argument/TimeStringArg.kt
@@ -6,7 +6,7 @@ import onl.tesseract.commandBuilder.CommandArgumentBuilderSteps
 class TimeStringArg(name: String) : CommandArgument<String>(name) {
 
     override fun define(builder: CommandArgumentBuilderSteps.Parser<String>) {
-        builder.parser { input, _ -> input }  // Renvoie l'input tel quel
+        builder.parser { input, _ -> input }
             .tabCompleter { input, _ ->
                 val timeUnits = listOf("second", "minute", "hour", "day", "month")
                 timeUnits.filter { it.startsWith(input.lowercase()) }
