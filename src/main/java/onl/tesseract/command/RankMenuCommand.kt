6d9378d3bf5ab1c2e9b3ea2a88a337db97381ec7
Florian Suchaud <41047704+aikiboo@users.noreply.github.com>
2024-11-29 00:05:51 +0100

    Staff commands (#9)
    * Création classe Staff.kt regroupant les commandes staff
    
    * Création PlayerRankCommandArg et StaffRankCommandArg
    
    * Création commande de gestion de grade : /staff rank player(staff) ...
    
    * Création commande de gestion de temps de jeu.
    ---------
    
    Co-authored-by: GuiguiShow <g.eckenschwiller@gmail.com>

--- src/main/java/onl/tesseract/command/RankMenuCommand.kt
+++ src/main/java/onl/tesseract/command/RankMenuCommand.kt
@@ -3,16 +3,16 @@ package onl.tesseract.command
 import onl.tesseract.commandBuilder.CommandContext
 import onl.tesseract.commandBuilder.annotation.Command
 import onl.tesseract.commandBuilder.annotation.CommandBody
-import onl.tesseract.menu.SpecialBlockMenu
+import onl.tesseract.menu.RankMenu
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 @Command
-class SpecialBlockCommand : CommandContext() {
+class RankMenuCommand : CommandContext() {
     @CommandBody
     fun onCommand(sender: CommandSender?): Boolean {
         if (sender is Player) {
-            SpecialBlockMenu(sender).open(sender)
+            RankMenu().open(sender)
         }
 
         return true
