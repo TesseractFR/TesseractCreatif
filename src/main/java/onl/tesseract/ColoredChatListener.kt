2516898ed774ae1068fb3b8001a0a0f4d7863793
GuiguiShow <g.eckenschwiller@gmail.com>
2025-02-23 23:51:48 +0100

    inclusion nick

--- src/main/java/onl/tesseract/ColoredChatListener.kt
+++ src/main/java/onl/tesseract/ColoredChatListener.kt
@@ -8,12 +8,14 @@ import onl.tesseract.core.event.ColoredChat
 import onl.tesseract.core.persistence.hibernate.boutique.TPlayerInfoService
 import onl.tesseract.rank.PlayerRankService
 import onl.tesseract.lib.service.ServiceContainer
+import onl.tesseract.nickname.NicknameService
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 
 class ColoredChatListener : Listener {
 
     private val rankService: PlayerRankService = ServiceContainer[PlayerRankService::class.java]
+    private val nicknameService: NicknameService = ServiceContainer[NicknameService::class.java]
 
     @EventHandler
     fun onPlayerChat(event: AsyncChatEvent) {
@@ -23,10 +25,17 @@ class ColoredChatListener : Listener {
         val titleDisplay = staffRank?.title ?: playerRank.title
 
         val gender = ServiceContainer[TPlayerInfoService::class.java][event.player.uniqueId].genre
+        val nickname = nicknameService.getNickname(event.player.uniqueId)
+
+        val displayNameComponent = if (nickname != null) {
+            ColoredChat.colorComponent(Component.text(nickname))
+        } else {
+            Component.text(event.player.name, color)
+        }
 
         val prefix = Component.text()
             .append(Component.text("[${titleDisplay.getDisplayName(gender)}] ", color, TextDecoration.BOLD))
-            .append(Component.text(event.player.name, color))
+            .append(displayNameComponent)
             .append(Component.text(" : ", NamedTextColor.WHITE))
             .build()
 
