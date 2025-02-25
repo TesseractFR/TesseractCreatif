9fcafb4c9c93a479acebc52bcb425f45bc6b8ebf
Guiguishow <165580884+Guiguishow@users.noreply.github.com>
2025-02-25 20:01:27 +0100

    Séparation speed walk et fly en 2 commandes (#28)

--- src/main/java/onl/tesseract/command/SpeedCommand.kt
+++ src/main/java/onl/tesseract/command/SpeedCommand.kt
@@ -6,29 +6,30 @@ import onl.tesseract.commandBuilder.annotation.Argument
 import onl.tesseract.commandBuilder.annotation.Command
 import onl.tesseract.commandBuilder.annotation.CommandBody
 import onl.tesseract.commandBuilder.annotation.Env
-import org.bukkit.attribute.Attribute
 import org.bukkit.entity.Player
 import kotlin.math.max
 import kotlin.math.min
 
-@Command(
-    name = "speed",
-    playerOnly = true,
-    args = [Argument(value = "speed", clazz = SpeedCommandArgument::class)])
+@Command(name = "speed", playerOnly = true)
 class SpeedCommand : CommandContext() {
 
+    private val defaultWalkSpeed: Float = 0.2f
     private val defaultFlySpeed: Float = 0.1f
 
-    @CommandBody
-    fun onCommand(@Env(key = "speed") speed: Float, sender: Player): Boolean {
-
+    @Command(name = "walk", args = [Argument(value = "speed", clazz = SpeedCommandArgument::class)])
+    fun walkSpeedCommand(@Env(key = "speed") speed: Float, sender: Player): Boolean {
         val normalizedSpeed = max(0.1f, min(speed, 10f))
+        sender.walkSpeed = (defaultWalkSpeed + (normalizedSpeed - 1f) * (1.0f - defaultWalkSpeed) / 9f)
+            .coerceAtMost(1.0f)
+        sender.sendMessage("§aVotre vitesse de marche a été réglée à $speed.")
+        return true
+    }
 
-        sender.walkSpeed = (0.2f + (normalizedSpeed - 1f) * (1.0f - 0.2f) / 9f).coerceAtMost(1.0f)
+    @Command(name = "fly", args = [Argument(value = "speed", clazz = SpeedCommandArgument::class)])
+    fun flySpeedCommand(@Env(key = "speed") speed: Float, sender: Player): Boolean {
+        val normalizedSpeed = max(0.1f, min(speed, 10f))
         sender.flySpeed = (defaultFlySpeed * normalizedSpeed).coerceAtMost(1.0f)
-
-        sender.sendMessage("§aVotre vitesse a été réglée à $speed.")
-
+        sender.sendMessage("§aVotre vitesse de vol a été réglée à $speed.")
         return true
     }
 }
