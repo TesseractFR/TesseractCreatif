8168c434de410fad8821820ddf866a2a3d1270f3
aikiboo <florian.suchaud@hotmail.fr>
2025-07-02 20:30:33 +0200

    Les scoreboards sont mis à jour et non plus recréés

--- src/main/java/onl/tesseract/creative/domain/scoreBoard/Board.kt
+++ src/main/java/onl/tesseract/creative/domain/scoreBoard/Board.kt
@@ -4,22 +4,25 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
-import org.bukkit.scoreboard.*
+import org.bukkit.scoreboard.Criteria
+import org.bukkit.scoreboard.DisplaySlot
+import org.bukkit.scoreboard.Objective
+import org.bukkit.scoreboard.Scoreboard
 
 open class Board(protected val player: Player) {
     lateinit var scoreboard: Scoreboard
     private lateinit var objective: Objective
 
     fun update() {
-        this.scoreboard = Bukkit.getScoreboardManager().newScoreboard
-        this.objective = scoreboard.registerNewObjective(
+        this.scoreboard = player.scoreboard
+        this.objective = scoreboard.getObjective("main") ?: scoreboard.registerNewObjective(
             "main",
             Criteria.DUMMY,
             Component.text("play.tesseract.onl", NamedTextColor.BLUE, TextDecoration.BOLD)
         )
+
         objective.displaySlot = DisplaySlot.SIDEBAR
         initScoreBoard()
     }
